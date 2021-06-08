27d484c496d3ba5d4ece1435edd45fa5f6c21524
Alexandr Barsukov <brskff@iMac-Sergio.localdomain>
2021-06-08 17:33:06 +0300

    Add GraphQLID type

--- schema/schema.js
+++ schema/schema.js
@@ -1,8 +1,16 @@
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = require('graphql')
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = require('graphql')
+
+const users = [
+    {id: '1', email: 'test@mail.ru', password: '123456'},
+    {id: '2', email: 'test2@mail.ru', password: '123456'},
+    {id: '3', email: 'test3@mail.ru', password: '123456'},
+    {id: '4', email: 'test4@mail.ru', password: '123456'},
+]
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: () => ({
+        id: {type: GraphQLID},
         email: {type: GraphQLString},
         password: {type: GraphQLString}
     })
@@ -13,12 +21,13 @@ const Query = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: {id: {type: GraphQLString}}
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return users.find(user => user.id === args.id)
+            }
         }
     },
-    resolve(parent, args) {
 
-    }
 })
 
 module.exports = new GraphQLSchema({
